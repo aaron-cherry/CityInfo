616c5e19cbe67fb5291554908b1378b4922c162d
Aaron Cherry <aaron.cherry10@gmail.com>
2025-03-02 18:00:18 -0800

    Added file upload functionality

--- CityInfo.API/Controllers/FilesController.cs
+++ CityInfo.API/Controllers/FilesController.cs
@@ -33,5 +33,27 @@ namespace CityInfo.API.Controllers
             var bytes = System.IO.File.ReadAllBytes(pathToFile);
             return File(bytes, contentType, Path.GetFileName(pathToFile));
         }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateFile(IFormFile file)
+        {
+            Console.WriteLine("start");
+            if (file.Length == 0 || file.Length > 20971520 || file.ContentType != "application/pdf")
+            {
+                return BadRequest("No file or invalid file has been inputted");
+            }
+            Console.WriteLine("isvalid check");
+
+            //WARNING BAD PRACTICE - JUST FOR FILE CREATION DEMONSTRATION ONLY - SECURITY VULNERABILITY
+            var path = Path.Combine(Directory.GetCurrentDirectory(), $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            Console.WriteLine("After upload to path");
+
+            return Ok("File upload succesful");
+        }
     }
 }
