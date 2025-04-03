4a20cd59075a016ec58df6b3e05f18f3f31c71c2
Aaron Cherry <aaron.cherry10@gmail.com>
2025-04-03 14:16:50 -0700

    Added poduction/development specific configurations

--- CityInfo.API/Services/LocalMailService.cs
+++ CityInfo.API/Services/LocalMailService.cs
@@ -2,9 +2,14 @@
 {
     public class LocalMailService : IMailService
     {
-        private string _mailTo = "admin@company.com";
-        private string _mailFrom = "noreply@company.com";
+        private string _mailTo = String.Empty;
+        private string _mailFrom = String.Empty;
 
+        public LocalMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
         public void Send(string subject, string message)
         {
             Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with {nameof(LocalMailService)}");
